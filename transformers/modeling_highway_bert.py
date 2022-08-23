e6f88ba79f06c32a377e5fa70761490fb7ecc093
jonsaadfalcon <jonsaadfalcon@gmail.com>
2022-08-23 16:07:25 -0400

    switch to roberta

--- transformers/modeling_highway_bert.py
+++ transformers/modeling_highway_bert.py
@@ -313,7 +313,7 @@ class BertModel(BertPreTrainedModel):
         print("embedding_output")
         print(type(embedding_output))
         print(embedding_output.shape) 
-        print(embedding_output) 
+        print(str(embedding_output))
 
         encoder_outputs = self.encoder(embedding_output,
                                        attention_mask=extended_attention_mask,
